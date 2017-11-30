6d58b6ffc05d65dfadfd22f0e18139ca3ffc242e
hoangddt <quochoangddt@gmail.com>
2017-12-01 01:03:20 +0700

    Send user name and info to server

--- server.js
+++ server.js
@@ -5,12 +5,28 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var port = 8089;
+var users = [];
 
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', function(socket) {
   console.log('new connection made');
 
+  socket.on('join', function (data) {
+      console.log(data);
+      console.log(users);
+      socket.nickname = data.nickname;
+      users[socket.nickname] = socket;
+
+      var userObj = {
+        nickname: data.nickname,
+        socket: socket.id
+      };
+
+
+      users.push(userObj);
+      io.emit('all-user', users);
+  })
 });
 
 server.listen(port, function() {
