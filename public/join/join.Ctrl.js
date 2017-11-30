afde8054265d34329e8909c3466eb7ceef7fafd2
hoangddt <quochoangddt@gmail.com>
2017-11-30 23:44:02 +0700

    Save user input name

--- public/join/join.Ctrl.js
+++ public/join/join.Ctrl.js
@@ -8,6 +8,14 @@
   JoinCtrl.$inject = ['$location', '$scope', '$localStorage', 'socket'];
 
   function JoinCtrl($location, $scope, $localStorage, socket) {
+    $scope.name = '';
+    var nickname;
 
+    $scope.join = function () {
+        nickname = $scope.name;
+        $localStorage.nickname = $scope.name;
+
+        $location.path('/main');
+    }
   }
 })();
